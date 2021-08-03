d272a9070122056ee6e9e33759acd2cb1ecb286a
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-04 00:08:34 +0600

    Added Even Number also

--- even-odd-number.js
+++ even-odd-number.js
@@ -1,14 +1,36 @@
 //Check Odd Number Function
 
 function isOddNumber(n) {
-        if (n % 2 == 1) {
-                return ("is ODD Number");
+        if (n == 0) {
+                return n + " is not valid value";
+        }
+        else if (n % 2 == 1) {
+                return (n + " is ODD Number");
         }
         else {
-                return ("is not and ODD Number");
+                return (n + " is an Even Number");
         }
 }
 
 //Example for testing purposes
-var number = 56;
-console.log(number, isOddNumber(number))
+var number = 29;
+console.log(isOddNumber(number))
+
+
+//Check Even Number Function
+
+function isEvenNumber(n) {
+        if (n == 0) {
+                return n + " is not a valid value";
+        }
+        if (n % 2 == 0) {
+                return (n + " is Even Number");
+        }
+        else {
+                return (n + " is Odd Number");
+        }
+}
+
+//Example for testing purposes
+
+console.log(isEvenNumber(50))
