0a8bb312ed375ca0802587554d77274c47cff50d
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-07 16:10:18 +0600

    Added Fibonacci Series

--- fibo.js
+++ fibo.js
@@ -5,3 +5,17 @@ for (var i = 2; i <= 10; i++) {
 }
 
 console.log(fibo)
+
+/*
+Create a Function
+For FibonacciSeries
+*/
+
+function fibonacciSeries(n) {
+        let fibo = [0, 1];
+        for (var i = 2; i <= n; i++) {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+        return fibo;
+}
+console.log(fibonacciSeries(12))
