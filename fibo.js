d58f2187a98020c7d8daaee6e143008919bfa028
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-08-07 16:25:08 +0600

    Added Fibonacci Validation Input

--- fibo.js
+++ fibo.js
@@ -12,10 +12,19 @@ For FibonacciSeries
 */
 
 function fibonacciSeries(n) {
-        let fibo = [0, 1];
-        for (var i = 2; i <= n; i++) {
-                fibo[i] = fibo[i - 1] + fibo[i - 2];
+
+        if (n < 2) {
+                return 'Please give a Valid number greater than 2'
         }
-        return fibo;
+        else if (typeof n == 'number') {
+                let fibo = [0, 1];
+                for (var i = 2; i <= n; i++) {
+                        fibo[i] = fibo[i - 1] + fibo[i - 2];
+                }
+                return fibo;
+        }
+        else {
+                return 'Please give a Valid number greater than 2'
+        }
 }
-console.log(fibonacciSeries(12))
+console.log(fibonacciSeries('-12'))
